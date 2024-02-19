a47499cd85f8ad7463e571355e7bb17eefd73966
Higor <higorjugler@hotmail.com>
2024-02-19 10:25:40 -0300

    Implementado lista com as imagens

--- JPApexPredators17/ApexPredators.swift
+++ JPApexPredators17/ApexPredators.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ApexPredators: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let type: PredatorType
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScenes]
+    let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    struct MovieScenes: Decodable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+    
+    enum PredatorType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land: .brown
+            case .air:  .teal
+            case .sea:  .blue
+            }
+        }
+    }
+}
--- JPApexPredators17/Predators.swift
+++ JPApexPredators17/Predators.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class Predators {
+    var apexPredators: [ApexPredators] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredators].self, from: data)
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+}
