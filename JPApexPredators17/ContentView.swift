308492dbf9a36af9ace64bf2f21536a5609cc9e8
Higor <higorjugler@hotmail.com>
2024-02-21 08:22:47 -0300

    Integrado toolbar para pesquisa

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -11,17 +11,12 @@ struct ContentView: View {
     let predators = Predators()
     
     @State var searchText = ""
+    @State var alphabetical = false
     
     var filteredDinos: [ApexPredators] {
-        if searchText.isEmpty {
-            return predators.apexPredators
-        } else {
-            return predators.apexPredators.filter {
-                predator in
-                predator.name.localizedCaseInsensitiveContains(searchText)
-                
-            }
-        }
+        predators.sort(by: alphabetical)
+        
+        return predators.search(for: searchText)
     }
     
     var body: some View {
@@ -60,6 +55,18 @@ struct ContentView: View {
             .searchable(text: $searchText)
             .autocorrectionDisabled()
             .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+                        Image(systemName: alphabetical ? "film" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+            }
             
         }
         .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
