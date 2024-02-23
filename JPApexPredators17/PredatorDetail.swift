9a8c6a30aa74f919ad6618947a10f51e9123a89e
Higor <higorjugler@hotmail.com>
2024-02-23 08:59:14 -0300

    Inicio da tela de info

---
+++ JPApexPredators17/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators17
+//
+//  Created by Higor Jugler on 2/23/24.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
