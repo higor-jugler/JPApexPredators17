6a6330da14458c3ab9883a3ed721e577f9518e5d
Higor <higorjugler@hotmail.com>
2024-02-23 10:07:27 -0300

    Dados da info implementados

--- JPApexPredators17/PredatorDetail.swift
+++ JPApexPredators17/PredatorDetail.swift
@@ -8,11 +8,70 @@
 import SwiftUI
 
 struct PredatorDetail: View {
+    let predator: ApexPredators
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                ZStack(alignment: .bottomTrailing)  {
+                    // Background image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // Dino image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width/1.5, height: geo.size.height/3)
+                        .scaleEffect(x: -1)
+                        .offset(y: 20)
+                }
+                VStack(alignment: .leading) {
+                    // Dino name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // Current location
+                    
+                    // Appears in
+                    Text("Appears in:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) {
+                        movie in
+                        Text("•" + movie)
+                    }
+                    // Movie moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    // Link to webpage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
+            }
+            .ignoresSafeArea()
+        }
     }
 }
 
 #Preview {
-    PredatorDetail()
+    PredatorDetail(predator: Predators().apexPredators[2])
+//        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
