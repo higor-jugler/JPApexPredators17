7c1edaa68da83925f778e2264a461c6a8c150970
Higor <higorjugler@hotmail.com>
2024-02-27 09:10:28 -0300

    Projeto finalizado

---
+++ JPApexPredators17/PredatorMap.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorMap.swift
+//  JPApexPredators17
+//
+//  Created by Higor Jugler on 2/27/24.
+//
+
+import SwiftUI
+
+struct PredatorMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorMap()
+}
